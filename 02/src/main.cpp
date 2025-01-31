d2aaf23e49d86d0115df842ce22eed4d07e197b9
Jim Jones <cthomas10101010@gmail.com>
2025-01-31 12:02:24 -0700

    prog 2 tested working

--- 02/src/main.cpp
+++ 02/src/main.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 #include <string>
 #include <array>
-#include "calculateDiscount.h"
-#include "determineLarger.h"
-#include "compareArrays.h"
-#include "sortArrays.h"
-#include "demoPtrTechniques.h"
+#include "CalculateDiscount.h"
+#include "DetermineLarger.h"
+#include "CompareArrays.h"
+#include "SortArrays.h"
+#include "DemoPtrTechniques.h"
 
-using std::cin;
 using std::cout;
+using std::cin;
 using std::endl;
 using std::string;
 using std::array;
 
-int main()
-{
+int main() {
     // 1) Function One - calculateDiscount
     double listPrice = 0.0;
     double discountPercent = 0.0;
     double discountedPrice = 0.0;
 
-    cout << "Enter the list price: ";
+    cout << "\n=== Discount Calculator ===\n";
+    cout << "Please enter the list price ($): ";
     cin >> listPrice;
-    cout << "Enter the discount percentage: ";
+    cout << "Please enter the discount percentage: ";
     cin >> discountPercent;
 
-    // call calculateDiscount
     calculateDiscount(listPrice, discountPercent, discountedPrice);
 
-    cout << "Discounted price is: " << discountedPrice << endl << endl;
+    cout << "Original price: $" << listPrice << endl;
+    cout << "After " << discountPercent << "% discount: $" << discountedPrice << endl;
+    cout << "You save: $" << (listPrice - discountedPrice) << endl << endl;
 
     // 2) Function Two - determineLarger
     int intOne = 0;
     int intTwo = 0;
-    int largerValue = 0; // will receive result via pointer
+    int largerValue = 0;
 
     cout << "Enter integer one: ";
     cin >> intOne;
@@ -44,63 +45,47 @@ int main()
     cout << "The larger of the two is: " << largerValue << endl << endl;
 
     // 3) Function Three - compareArrays
-    // We'll create two built-in arrays of the same size, and ask user for data.
     const int SIZE = 3;
-    std::string arr1[SIZE];
-    std::string arr2[SIZE];
+    string arr1[SIZE];
+    string arr2[SIZE];
 
     cout << "Enter 3 strings for arr1:\n";
-    for (int i = 0; i < SIZE; ++i)
-    {
+    for (int i = 0; i < SIZE; ++i) {
         cin >> arr1[i];
     }
 
     cout << "Enter 3 strings for arr2:\n";
-    for (int i = 0; i < SIZE; ++i)
-    {
+    for (int i = 0; i < SIZE; ++i) {
         cin >> arr2[i];
     }
 
     bool arraysMatch = compareArrays(arr1, arr2, SIZE);
-    if (arraysMatch)
-    {
-        cout << "The two arrays have identical data.\n";
-    }
-    else
-    {
-        cout << "The two arrays do NOT match.\n";
-    }
-    cout << endl;
+    cout << "The two arrays do " << (arraysMatch ? "" : "NOT ") << "match.\n\n";
 
     // 4) Function Four - sortArrays
-    // Example built-in array (size 5) and std::array (size 5).
-    std::string builtInArr[5];
+    string builtInArr[5];
     array<string, 5> stdArr;
 
     cout << "Enter 5 strings for builtInArr:\n";
-    for (int i = 0; i < 5; ++i)
-    {
+    for (int i = 0; i < 5; ++i) {
         cin >> builtInArr[i];
     }
 
     cout << "Enter 5 strings for stdArr:\n";
-    for (int i = 0; i < 5; ++i)
-    {
+    for (int i = 0; i < 5; ++i) {
         cin >> stdArr[i];
     }
 
     sortArrays(builtInArr, 5, stdArr);
 
     cout << "builtInArr after sort:\n";
-    for (int i = 0; i < 5; ++i)
-    {
+    for (int i = 0; i < 5; ++i) {
         cout << builtInArr[i] << " ";
     }
     cout << endl;
 
     cout << "stdArr after sort:\n";
-    for (const auto &s : stdArr)
-    {
+    for (const auto &s : stdArr) {
         cout << s << " ";
     }
     cout << endl << endl;
